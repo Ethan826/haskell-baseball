477a10df06ea0fadcd70c4919b4de24e189e3d62
Ethan Kent <ekent@spoonflower.com>
2022-06-18 08:55:42 -0700

    WIP

--- app/Count.hs
+++ app/Count.hs
@@ -1,25 +1,56 @@
 module Count where
 
-incrementBalls :: Balls -> Balls
-incrementBalls BallThree = BallZero
-incrementBalls balls = succ balls
+import Main (Pitch (..))
 
 data Balls
   = BallZero
   | BallOne
   | BallTwo
   | BallThree
-  deriving (Show, Eq, Ord, Bounded, Enum)
+  | Walk
+  deriving (Show, Eq, Ord, Enum, Bounded)
+
+incrementBalls :: Balls -> Either String Balls
+incrementBalls Walk = Left "Can't increment walk"
+incrementBalls b = pure $ succ b
+
+reduceBalls :: Pitch -> Balls -> Either String Balls
+reduceBalls p = case p of
+  Ball -> incrementBalls
+  IntentionalBall -> incrementBalls
+  BallPitcherWentToMouth -> incrementBalls
+  BallInPlayByBatter -> incrementBalls
+  BallInPlayOnPitchout -> incrementBalls
+  _ -> pure
 
 data Strikes
   = StrikeZero
   | StrikeOne
   | StrikeTwo
-  deriving (Show, Eq, Ord, Enum)
+  | Strikeout
+  deriving (Show, Eq, Ord, Enum, Bounded)
 
-incrementStrikes :: Strikes -> Strikes
-incrementStrikes StrikeTwo = StrikeZero
-incrementStrikes strikes = succ strikes
+incrementStrikes :: Strikes -> Either String Strikes
+incrementStrikes Strikeout = Left "Can't increment strikeout"
+incrementStrikes s = pure $ succ s
+
+incrementStrikesOnFoul :: Strikes -> Either String Strikes
+incrementStrikesOnFoul StrikeTwo = pure StrikeTwo
+incrementStrikesOnFoul s = incrementStrikes s
+
+reduceStrikes :: Pitch -> Strikes -> Either String Strikes
+reduceStrikes p = case p of
+  CalledStrike -> incrementStrikes
+  StrikeUnknown -> incrementStrikes
+  MissedBuntAttempt -> incrementStrikes
+  FoulBunt -> incrementStrikes
+  SwingingOnPitchout -> incrementStrikes
+  FoulTipOnBunt -> incrementStrikes
+  SwingingStrike -> incrementStrikes
+  Foul -> incrementStrikesOnFoul
+  FoulBallOnPitchout -> incrementStrikesOnFoul
+  FoulTip -> incrementStrikesOnFoul
+  _ -> pure
 
 data Count = Count
   { balls :: Balls
